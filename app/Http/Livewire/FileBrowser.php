3495181385c2347e32126a545e3afb0a69ef54a8
martin <martin@gmx.at>
2021-04-29 16:23:37 +0200

    create Folder

--- app/Http/Livewire/FileBrowser.php
+++ app/Http/Livewire/FileBrowser.php
@@ -10,6 +10,29 @@ class FileBrowser extends Component
 
     public $ancestors;
 
+    public $creatingNewFolder = false;
+
+    public $newFolderState = [
+        'name' => ''
+    ];
+
+    public function createFolder()
+    {
+        $this->validate([
+            'newFolderState.name' => 'required|max:255'
+        ]);
+
+        $object = $this->currentTeam->objects()->make(['parent_id' => $this->object->id]);
+        $object->objectable()->associate($this->currentTeam->folders()->create($this->newFolderState));
+        $object->save();
+
+    }
+
+    public function getCurrentTeamProperty()
+    {
+        return auth()->user()->currentTeam;
+    }
+
     public function render()
     {
         return view('livewire.file-browser');
--- resources/views/livewire/file-browser.blade.php
+++ resources/views/livewire/file-browser.blade.php
@@ -6,7 +6,7 @@
                     <input class="w-full border-2 border-gray-300 h-12" type="search" placeholder="Suche nach Dateien und Ordnern">
                 </div>
                 <div class="mt-4 sm:mt-0">
-                    <button class="bg-gray-200 h-12 px-6 mr-2 mb-2 sm:mb-0">
+                    <button class="bg-gray-200 h-12 px-6 mr-2 mb-2 sm:mb-0" wire:click="$set('creatingNewFolder', true)">
                         Neuer Ordner
                     </button>
                     <button class="bg-blue-600 h-12 px-6 text-white font-bold">
@@ -33,44 +33,59 @@
                 </div>
             </div>
         </div>
+        @if($creatingNewFolder)
+            <div class="bg-white overflow-hidden shadow-md p-6 mt-1 hover:bg-blue-300">
+                <form class="flex items-center flex-wrap sm:flex-nowrap" wire:submit.prevent="createFolder">
+                    <input type="text" class="w-full border-2 border-gray-300 h-12 sm:mr-3 mb-3 sm:mb-0" wire:model="newFolderState.name">
+                    <div class="flex-none">
+                        <button type="submit" class="whitespace-nowrap bg-blue-600 h-12 px-6 text-white font-bold mr-2">
+                            Ordner anlegen
+                        </button>
+                    </div>
+                    <button wire:click="$set('creatingNewFolder', false)" type="submit" class="bg-gray-200 h-12 px-6 font-bold">
+                        Abbrechen
+                    </button>
+                </form>
+            </div>
+        @endif
         @foreach($object->children as $child)
-        <div class="bg-white overflow-hidden shadow-md p-6 mt-1 hover:bg-blue-300">
-            <div class="flex items-center justify-between flex-wrap px-3">
-                <div class="flex items-center w-2/5 p-3">
+            <div class="bg-white overflow-hidden shadow-md p-6 mt-1 hover:bg-blue-300">
+                <div class="flex items-center justify-between flex-wrap px-3">
+                    <div class="flex items-center w-2/5 p-3">
+                        @if($child->objectable_type === 'file')
+                            <svg xmlns="http://www.w3.org/2000/svg"   viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="text-blue-700 feather feather-file-text w-7 h-7 mx-1"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
+                        @endif
+
+                        @if($child->objectable_type === 'folder')
+                            <svg xmlns="http://www.w3.org/2000/svg"   viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="text-blue-700 feather feather-folder w-7 h-7 mx-1"><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+                        @endif
+
+                        @if($child->objectable_type === 'folder')
+                            <a href="{{ route('files', ['uuid' => $child->uuid]) }}" class="font-bold text-lg text-blue-600">
+                                {{ $child->objectable->name }}
+                            </a>
+                        @endif
+
+                        @if($child->objectable_type === 'file')
+                            <a href="" class="font-bold text-lg text-blue-600">
+                                {{ $child->objectable->name }}
+                            </a>
+                        @endif
+                    </div>
                     @if($child->objectable_type === 'file')
-                        <svg xmlns="http://www.w3.org/2000/svg"   viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="text-blue-700 feather feather-file-text w-7 h-7 mx-1"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
+                    <div class="p-3">{{ $child->objectable->size }}</div>
+                    @else
+                    <div class="p-3">&mdash;</div>
                     @endif
-
-                    @if($child->objectable_type === 'folder')
-                        <svg xmlns="http://www.w3.org/2000/svg"   viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="text-blue-700 feather feather-folder w-7 h-7 mx-1"><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
-                    @endif
-
-                    @if($child->objectable_type === 'folder')
-                        <a href="{{ route('files', ['uuid' => $child->uuid]) }}" class="font-bold text-lg text-blue-600">
-                            {{ $child->objectable->name }}
-                        </a>
-                    @endif
-
-                    @if($child->objectable_type === 'file')
-                        <a href="" class="font-bold text-lg text-blue-600">
-                            {{ $child->objectable->name }}
-                        </a>
-                    @endif
-                </div>
-                @if($child->objectable_type === 'file')
-                <div class="p-3">{{ $child->objectable->size }}</div>
-                @else
-                <div class="p-3">&mdash;</div>
-                @endif
-                <div class="p-3">{{ $child->created_at }}</div>
-                <div class="p-3">
-                    <div class="">
-                        <button class="mr-2 bg-gray-200 h-7 px-3">Umbennenen</button>
-                        <button class="text-white font-bold bg-red-600 h-7 px-3">Löschen</button>
+                    <div class="p-3">{{ $child->created_at }}</div>
+                    <div class="p-3">
+                        <div class="">
+                            <button class="mr-2 bg-gray-200 h-7 px-3">Umbennenen</button>
+                            <button class="text-white font-bold bg-red-600 h-7 px-3">Löschen</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
         @endforeach
 
         @if($object->children->count() === 0)
